928dcaab1aa4f8125880cb2de6399d9626b16819
Bosco-Wong <142552491+Bosco-Wong@users.noreply.github.com>
2024-04-22 20:50:54 -0700

    Update APLine.java

--- src/main/java/APLine.java
+++ src/main/java/APLine.java
@@ -1,5 +1,18 @@
 public class APLine
 {
-  /* your code here */
+  private int a, b, c;
+  Public APLine (int x, int y, int slope) {
+    a = x;
+    b = y;
+    c = slope;
+  }
+  public int getSlope(){
+    return -(a/b);
+  }
+  public boolean isOnLine(int x, int y){
+    if(a*x + b*y + c == 0)
+      return true;
+    return false;
+  }
   
 }
